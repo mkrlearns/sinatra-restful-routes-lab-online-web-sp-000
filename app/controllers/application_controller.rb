a93e633ca6c5edb8e83052df6b5159a2689e4052
ryan m <contact@ryanmeek.com>
2020-03-01 15:37:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,20 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get('/recipes') {@recipes = Recipe.all; erb :index}
+  get('/recipes/new') {erb :new}
+  get('/recipes/:id') {@recipes = Recipe.find_by_id(params[:id]); erb :show}
+  get('/recipes/:id/edit') {@recipes = Recipe.find_by_id(params[:id]); erb :edit}
+  post('/recipes') {@recipes = Recipe.create(params); redirect to "/recipes/#{@recipe.id}"}
+  delete('/recipes/:id') {Recipe.find_by_id(params[:id]).delete; redirect to "/recipes"}
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
 
 end
